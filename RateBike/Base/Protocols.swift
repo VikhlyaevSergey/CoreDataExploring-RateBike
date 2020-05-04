942af0a4766acd169ae7d1035090544f950e8735
Сергей Вихляев <s.vikhlyaev@rendez-vous.ru>
2020-05-04 18:46:12 +0300

    First commit

---
+++ RateBike/Base/Protocols.swift
@@ -0,0 +1,14 @@
+//
+//  Protocols.swift
+//  RateBike
+//
+//  Created by Сергей Вихляев on 04.05.2020.
+//  Copyright © 2020 Сергей Вихляев. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+protocol BaseConfiguratorProtocol {
+    func configure(viewController: UIViewController, navigationController: UINavigationController?)
+}
